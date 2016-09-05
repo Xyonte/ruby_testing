19275b5e6a88b0d382d2fb509e832d9c03e4520b
Evan <Gregwareev@gmail.com>
2016-09-05 16:03:49 -0400

    Finished Project

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,27 @@
 class Book
-# write your code here
+	attr_accessor :title
+	
+	def initialize()
+	end
+	
+	def title()
+		conjunctions = ['for', 'and', 'nor', 'but', 'or', 'yet', 'so']
+		prepositions = ['in', 'of', 'the']
+		articles = ['a', 'an']
+		words = @title.split(' ')
+		full_title = ""
+		#split title into individual words
+		count = 0
+		words.each do |segment|
+			if !(conjunctions.include? segment or prepositions.include? segment or articles.include? segment and count != 0)
+				full_title += segment.capitalize + " "
+			else
+				full_title += segment + " "
+			end
+			count +=1
+		end
+		#capitalize each word indiscriminately
+
+		full_title.chop
+	end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,37 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  
+  def initalize
+
+  end
+  def seconds
+  	if @seconds == nil
+  		0
+  	else
+  		@seconds
+  	end
+
+  end
+  def double_digits(number)
+  	while number >= 60
+  		number = number - 60
+  	end
+  	if number < 10
+  		"0" + number.to_s
+  	else
+  		number.to_s
+  	end
+
+  end
+
+  def time_string
+  	secs = @seconds%60
+  	mins = @seconds/60
+  	hours = mins/60
+  	secs = double_digits(secs)
+  	mins = double_digits(mins)
+  	hours = double_digits(hours)
+
+  	hours + ":" + mins + ":" + secs
+  end
 end
